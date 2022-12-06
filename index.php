743b2f41b767305a273e172a2dd58afe25441585
Jasmine <116144441+jas-dav@users.noreply.github.com>
2022-12-05 20:00:49 -0800

    pre-populated set option based on cookie

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+    if(isset($_COOKIE['fname'])){
+    echo 'Thanks for booking with us '.$_COOKIE['fname']. '!<br>'; 
+    
+    
+    } 
+?>
+
+
 <html lang="en">
   <head>
     <meta charset="UTF-8">
@@ -37,41 +47,235 @@
       <div id="reserve" ">
         <em>Please fill out the following to reserve your order:</em>
       </div>
-      
+
+
+    <?php
+    // IF LAYERED ARCH WAS SELECTED PREVIOUSLY (from cookie) pre-select Layered Arch ---------
+    if (isset($_COOKIE['set']) && $_COOKIE['set'] == "Layered Arch") {
+    ?>
     <div class="container">
-      <form class="center" action="packages.php" method="post" id="setForm">
-        <div class="row">
-          <div class="col-4">
-            <input type="radio" id="lArch" name="set" value="Layered Arch">
-            <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
-            <p class="text-center">Layered Arch</p>
-          </div>
-          <div class="col-4">
-            <input type="radio" id="modernRound" name="set" value="Modern Round">
-            <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
-            <p class="text-center">Modern Round</p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4">
-            <input type="radio" id="vintageMirror" name="set" value="Vintage Mirror">
-            <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
-            <p class="text-center">Vintage Mirror</p>
-          </div>
-          <div class="col-4">
-            <input type="radio" id="DarkWalnut" name="set" value="Dark Walnut">
-            <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
-            <p class="text-center">Dark Walnut</p>
-          </div>
-        </div>
-        <div class="row-cols-4" id="rustic">
-          <input type="radio" id="rusticWood" name="set" value="Rustic Wood">
-          <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            <br>
-          <p class="text-center">Rustic Wood</p>
-        </div>
+        <form class="center" action="packages.php" method="post" id="setForm">
+            <div class="row">
+                <div class="col-4">
+                    <input type="radio" checked id="lArch" name="set" value="Layered Arch">
+                    <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
+                    <p class="text-center">Layered Arch</p>
+                </div>
+                <div class="col-4">
+                    <input type="radio" id="modernRound" name="set" value="Modern Round">
+                    <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
+                    <p class="text-center">Modern Round</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <input type="radio" id="vintageMirror" name="set" value="Vintage Mirror">
+                    <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
+                    <p class="text-center">Vintage Mirror</p>
+                </div>
+                <div class="col-4">
+                    <input type="radio" id="DarkWalnut" name="set" value="Dark Walnut">
+                    <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
+                    <p class="text-center">Dark Walnut</p>
+                </div>
+            </div>
+            <div class="row-cols-4" id="rustic">
+                <input type="radio" id="rusticWood" name="set" value="Rustic Wood">
+                <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            
+                <br>
+                <p class="text-center">Rustic Wood</p>
+            </div>
+
+        <?php }
+        // IF MODERN ROUND WAS SELECTED PREVIOUSLY (from cookie) pre-select Modern round---------
+        if (isset($_COOKIE['set']) && $_COOKIE['set'] == "Modern Round") { ?>
+
+            <div class="container">
+                <form class="center" action="packages.php" method="post" id="setForm">
+                    <div class="row">
+                    <div class="col-4">
+                        <input type="radio"  id="lArch" name="set" value="Layered Arch">
+                        <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
+                        <p class="text-center">Layered Arch</p>
+                    </div>
+                <div class="col-4">
+                    <input type="radio" checked id="modernRound" name="set" value="Modern Round">
+                    <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
+                    <p class="text-center">Modern Round</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <input type="radio" id="vintageMirror" name="set" value="Vintage Mirror">
+                    <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
+                    <p class="text-center">Vintage Mirror</p>
+                </div>
+                <div class="col-4">
+                    <input type="radio" id="DarkWalnut" name="set" value="Dark Walnut">
+                    <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
+                    <p class="text-center">Dark Walnut</p>
+                </div>
+            </div>
+            <div class="row-cols-4" id="rustic">
+                <input type="radio" id="rusticWood" name="set" value="Rustic Wood">
+                <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            <br>
+                <p class="text-center">Rustic Wood</p>
+            </div>
+
+        <?php }
+        // IF VINTAGE MIRROR WAS SELECTED PREVIOUSLY (from cookie) pre-select Vintage Mirror---------
+        if (isset($_COOKIE['set']) && $_COOKIE['set'] == "Vintage Mirror") { ?>
+            <div class="container">
+                <form class="center" action="packages.php" method="post" id="setForm">
+                    <div class="row">
+                        <div class="col-4">
+                            <input type="radio"  id="lArch" name="set" value="Layered Arch">
+                            <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
+                            <p class="text-center">Layered Arch</p>
+                        </div>
+                        <div class="col-4">
+                            <input type="radio" id="modernRound" name="set" value="Modern Round">
+                            <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
+                            <p class="text-center">Modern Round</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <input type="radio" checked id="vintageMirror" name="set" value="Vintage Mirror">
+                            <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
+                            <p class="text-center">Vintage Mirror</p>
+                        </div>
+                        <div class="col-4">
+                            <input type="radio" id="DarkWalnut" name="set" value="Dark Walnut">
+                            <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
+                            <p class="text-center">Dark Walnut</p>
+                        </div>
+                    </div>
+                    <div class="row-cols-4" id="rustic">
+                        <input type="radio" id="rusticWood" name="set" value="Rustic Wood">
+                        <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            <br>
+                        <p class="text-center">Rustic Wood</p>
+                    </div>
+
+
+        <?php }
+        // IF DARK WALNUT WAS SELECTED PREVIOUSLY (from cookie) pre-select DARK WALNUT---------
+        if (isset($_COOKIE['set']) && $_COOKIE['set'] == "Dark Walnut") { ?>
+            <div class="container">
+            <form class="center" action="packages.php" method="post" id="setForm">
+            <div class="row">
+                <div class="col-4">
+                    <input type="radio"  id="lArch" name="set" value="Layered Arch">
+                    <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
+                    <p class="text-center">Layered Arch</p>
+                </div>
+                <div class="col-4">
+                    <input type="radio" id="modernRound" name="set" value="Modern Round">
+                    <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
+                    <p class="text-center">Modern Round</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <input type="radio"  id="vintageMirror" name="set" value="Vintage Mirror">
+                    <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
+                    <p class="text-center">Vintage Mirror</p>
+                </div>
+                <div class="col-4">
+                    <input type="radio" checked id="DarkWalnut" name="set" value="Dark Walnut">
+                    <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
+                    <p class="text-center">Dark Walnut</p>
+                </div>
+            </div>
+            <div class="row-cols-4" id="rustic">
+                <input type="radio" id="rusticWood" name="set" value="Rustic Wood">
+                <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            
+                <br>
+                <p class="text-center">Rustic Wood</p>
+            </div>
+
+
+            <?php }
+
+            if (isset($_COOKIE['set']) && $_COOKIE['set'] == "Rustic Wood") { ?>
+            <div class="container">
+                <form class="center" action="packages.php" method="post" id="setForm">
+                    <div class="row">
+                        <div class="col-4">
+                            <input type="radio"  id="lArch" name="set" value="Layered Arch">
+                            <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
+                            <p class="text-center">Layered Arch</p>
+                        </div>
+                        <div class="col-4">
+                            <input type="radio" id="modernRound" name="set" value="Modern Round">
+                            <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
+                            <p class="text-center">Modern Round</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <input type="radio" checked id="vintageMirror" name="set" value="Vintage Mirror">
+                            <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
+                            <p class="text-center">Vintage Mirror</p>
+                        </div>
+                        <div class="col-4">
+                            <input type="radio" id="DarkWalnut" name="set" value="Dark Walnut">
+                            <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
+                            <p class="text-center">Dark Walnut</p>
+                        </div>
+                    </div>
+                    <div class="row-cols-4" id="rustic">
+                        <input type="radio" checked id="rusticWood" name="set" value="Rustic Wood">
+                        <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            
+                        <br>
+                        <p class="text-center">Rustic Wood</p>
+                    </div>
+
+
+        <?php }
+        // IF COOKIE['set'] IS NOT SET - NO PRE-SELECTED OPTIONS
+        if (!isset($_COOKIE['set'])) { ?>
+        <div class="container">
+          <form class="center" action="packages.php" method="post" id="setForm">
+            <div class="row">
+              <div class="col-4">
+                <input type="radio" id="lArch" name="set" value="Layered Arch">
+                <label for="lArch"><img src = "photos/JM1_5196.jpg"  width = "100%" alt = "Layered Arch"></label><br>
+                <p class="text-center">Layered Arch</p>
+              </div>
+              <div class="col-4">
+                <input type="radio" id="modernRound" name="set" value="Modern Round">
+                <label for="modernRound"><img src = "photos/IMG_7338.jpg"  width = "100%" alt = "Modern Round"></label><br>
+                <p class="text-center">Modern Round</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-4">
+                <input type="radio" id="vintageMirror" name="set" value="Vintage Mirror">
+                <label for="vintageMirror"><img src = "photos/_DSC0756.jpg"  width = "100%" alt = "Vintage Mirror"></label><br>
+                <p class="text-center">Vintage Mirror</p>
+              </div>
+              <div class="col-4">
+                <input type="radio" id="DarkWalnut" name="set" value="Dark Walnut">
+                <label for="DarkWalnut" id="walnut"><img src = "photos/B98C3F0B-7C55-4FE1-A2ED-8698DA929605.jpg"  width = "140%" alt = "Dark Walnut"></label><br>
+                <p class="text-center">Dark Walnut</p>
+              </div>
+            </div>
+            <div class="row-cols-4" id="rustic">
+              <input type="radio" id="rusticWood" name="set" value="Rustic Wood">
+              <label for="rusticWood"><img src = "photos/Donnie+Rosie+Photo-9449.jpg"  width = "200%" alt = "Rustic Wood"></label>            
+              <br>
+              <p class="text-center">Rustic Wood</p>
+            </div>
+
+              <?php } ?>
         
+         <!--pre-populated calendar date value based on cookie input, and made input min = today; that way user is unable to book date in the past -->
         <br><label for="date">Pick Your Date: </label><br>
-        <input type="date" name="date" min="<?= date('Y-m-d'); ?>">
+        <input type="date" name="date" value="<?php echo date($_COOKIE['date']); ?>" min="<?= date('Y-m-d'); ?>">
+        
+        
+
 
         <br><br><input type="submit" id="button"><br><br>
         
@@ -79,5 +283,6 @@
       </form>
     </div>
     
+
   </body>
 </html>
